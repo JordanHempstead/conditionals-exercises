3923f2305acc105ac9ab599febd7cdd58050045c
JordanHempstead <tariqhempst@gmail.com>
2018-03-29 14:09:38 -0700

    Add files via upload

---
+++ conditionals_exercise-01.py
@@ -0,0 +1,12 @@
+#Python MC - Conditionals - Exercise 01
+
+guess = input ("Do you want to go left or right? ")
+
+if guess == "left":
+    print ("Ok let's go left!")
+
+elif guess == "right":
+    print ("Ok we are going right!")
+
+else:
+    print ("Hmmm I didn't understand. I guess we'll stop")
